6f1b49495f99861bedf85191b747792224587721
hanjialin <hanjialin1991@hotmail.com>
2025-03-11 22:12:09 +0800

    格式化了一下代码 注释掉了可视化大屏缩放组件

--- src/components/AppProvider/AppProvider.vue
+++ src/components/AppProvider/AppProvider.vue
@@ -5,19 +5,17 @@
     :theme="getDarkTheme"
     :theme-overrides="themeOverrides"
   >
-    <!--    <ScreenAdaptation height="1080">-->
-    <v-screen-adaptation mode="auto" height="1080" :scale="scale">
-      <n-loading-bar-provider>
-        <n-dialog-provider>
-          <n-notification-provider>
-            <n-message-provider>
-              <App />
-            </n-message-provider>
-          </n-notification-provider>
-        </n-dialog-provider>
-      </n-loading-bar-provider>
-    </v-screen-adaptation>
-    <!--    </ScreenAdaptation>-->
+    <!--    <v-screen-adaptation mode="auto" height="1080" :scale="scale">-->
+    <n-loading-bar-provider>
+      <n-dialog-provider>
+        <n-notification-provider>
+          <n-message-provider>
+            <App />
+          </n-message-provider>
+        </n-notification-provider>
+      </n-dialog-provider>
+    </n-loading-bar-provider>
+    <!--    </v-screen-adaptation>-->
   </n-config-provider>
 </template>
 
@@ -31,17 +29,13 @@ import { darkTheme } from 'naive-ui'
 import { computed } from 'vue'
 import 'v-screen-adaptation/dist/v-screen-adaptation.css'
 import VScreenAdaptation from 'v-screen-adaptation'
-// import ScreenAdaptation from '@/components/ScreenAdaptation/ScreenAdaptation.vue'
 const { zhCN, dateZhCN, theme, changeThemeOs } = useConfig() //theme 使用默认主题配置
 const designStore = useDesignSettingStore()
 const getDarkTheme = computed(() => (designStore.darkTheme === 'dark' ? darkTheme : null))
 // changeThemeOs() //获取系统主题,并改变theme的值
 const locale = zhCN
 const dateLocale = dateZhCN
-const scale = ref(1)
-setTimeout(() => {
-  scale.value = 2.6
-}, 1000)
+
 // const osTheme = theme
 const themeOverrides = computed(() => {
   const appTheme = designSetting.appTheme
